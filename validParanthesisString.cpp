8c4313397490cac6e2b1375bc5387332c21f2dce
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2024-08-24 22:06:42 +0530

    Create validParanthesisString.cpp

---
+++ validParanthesisString.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool checkValidString(string s) {
+       stack<int> open,star;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==')'){
+                if(open.empty()){
+                    if(star.empty()) return false;
+                    else star.pop();
+                }
+                else open.pop();
+            }
+            else if(s[i]=='('){
+                open.push(i);
+            }
+            else star.push(i);
+        }
+        while(!open.empty()){
+            if(star.empty() || open.top()>star.top()) return false;
+            else {
+                star.pop();
+                open.pop();
+            }
+        }
+        return true;
+    }

5af27962f555ba5ef9bad0aaf36ab793dba41ed4
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2024-08-25 22:53:51 +0530

    Create arrayStateAfterKmul1.cpp

---
+++ arrayStateAfterKmul1.cpp
@@ -0,0 +1,13 @@
+vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        while(k>0){
+            int mini = *min_element(nums.begin(),nums.end());
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]==mini){
+                    nums[i] = nums[i]*multiplier;
+                    break;
+                }
+            }
+            k--;
+        }
+        return nums;
+    }
